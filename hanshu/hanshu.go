ffc1dec3ac31f1a45a1cadd14563b2746f167dc1
Apang <ss@babybus.com>
2023-04-10 00:38:58 +0800

    加入函数

---
+++ hanshu/hanshu.go
@@ -0,0 +1,125 @@
+package hanshu
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+//函数的第一个字母大写
+
+//函数定义
+//函数声明
+//func 函数名(参数)(返回值){
+//	函数体
+//}
+
+//一个普通的函数
+func Add(x, y int) int {
+	return x + y
+}
+func Sub(x, y int) int {
+	return x - y
+}
+
+//可变参数
+//可变参数变得是参数的数量而不是参数的类型
+func KbADD(x ...int) int {
+	sum := 0
+	for _, i := range x {
+		sum += i
+	}
+	return sum
+}
+
+//对象数组
+type Person struct {
+	Name string
+	Sex  string
+	Age  int
+}
+
+//高阶函数：函数本身作为函数的参数或返回值
+//作为参数
+func AddPro(x, y int, op func(int, int) int) int {
+	return op(x, y)
+}
+
+//作为返回值
+func Switchpro(s string) (func(int, int) int, error) {
+	switch s {
+	case "+":
+		return Add, nil
+	case "-":
+		return Sub, nil
+	default:
+		err := errors.New("无法识别的操作符")
+		return nil, err
+	}
+}
+
+//匿名函数
+//匿名函数声明
+//func(参数)(返回值){
+//	函数体
+//}
+//因为匿名函数没有函数名，所以匿名函数只能在某个变量或者函数中
+//匿名函数多用于实现回调函数和闭包.
+func Nm() {
+	//匿名函数寄存在变量中，此时add相当于变量名，但只在Nm函数中可以调用。
+	add := func(x, y int) int {
+		return x + y
+	}
+	sum := add(20, 30)
+	fmt.Println(sum)
+	//自执行函数：匿名函数定义完加()直接执行
+	func(x, y int) {
+		fmt.Println(x + y)
+	}(50, 20)
+}
+
+//闭包
+//闭包指的是一个函数和与其相关的引用环境组合而成的实体。简单来说，闭包=函数+引用环境
+func Adder() func(int) int {
+	var x int
+	fmt.Println("x=:", x)
+	return func(y int) int {
+		fmt.Println("y=:", y)
+		x += y
+		return x
+	}
+}
+
+//闭包进阶1
+func adder2(x int) func(int) int {
+	return func(y int) int {
+		x += y
+		return x
+	}
+}
+
+//闭包进阶2
+func MakeSuffixFunc(suffix string) func(string) string {
+	return func(name string) string {
+		//strings.HasSuffix是strings包中的函数，用于检查字符串是否以指定的后缀结尾。 其传入参数为(string,string)两个字符串,返回值为bool一个布尔类型.
+		//假如我传入x和y(strings.HasSuffix(x, y)),如果x的后缀为y，那么返回true；否则为false
+		if !strings.HasSuffix(name, suffix) {
+			return "照片类型-错误"
+		}
+		return "照片类型-正确"
+	}
+}
+
+//闭包进阶3
+func Calc(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		base += i
+		return base
+	}
+
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	return add, sub
+}
--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"study/array"
+	"fmt"
+	"study/hanshu"
 )
 
 func main() {
@@ -18,5 +19,62 @@ func main() {
 	//tiaozhuan.BreakDemo()
 	//tiaozhuan.ContinuDemo()
 	////数组
-	array.Array()
+	//array.Array()
+	// 普通函数
+	//fmt.Println(hanshu.Add(10, 20))
+	//可变参数
+	//fmt.Println(hanshu.KbADD(10, 20, 30))
+	//对象数组
+	//c := make([]hanshu.Person, 0, 100)
+	//c = append(c, hanshu.Person{"apang", "man", 22})
+	//c = append(c, hanshu.Person{"Lcs", "nan", 23})
+	//c = append(c, hanshu.Person{"Mina", "woman", 20})
+	//for _, x := range c {
+	//	if x.Name == "Lcs" {
+	//		fmt.Println(x)
+	//		break
+	//	}
+	//}
+	//高阶函数
+	//作为参数
+	//fmt.Println(hanshu.AddPro(10, 20, hanshu.Add))
+	//作为返回值
+	//fmt.Println(hanshu.Switchpro("+"))
+
+	//匿名函数
+	//hanshu.Nm()
+
+	//闭包
+	//f := hanshu.Adder()
+	//fmt.Println(f(10))
+	//fmt.Println(f(20))
+	//
+	//f1 := hanshu.Adder()
+	//fmt.Println(f1(30))
+	//fmt.Println(f1(40))
+	//闭包进阶2
+	//hz := hanshu.MakeSuffixFunc(".png")
+	//println(hz("1.png"))
+	//println(hz("2.png"))
+	//println(hz("3.jpg"))
+	//println(hz("4.png"))
+	//println(hz("5.jpg"))
+	//output
+	//照片类型-正确
+	//照片类型-正确
+	//照片类型-错误
+	//照片类型-正确
+	//照片类型-错误
+
+	//闭包进阶3
+	add1, sub1 := hanshu.Calc(10)
+	fmt.Println(add1(5))
+	fmt.Println(sub1(3))
+	fmt.Println(add1(4))
+	fmt.Println(sub1(2))
+	//output
+	//15
+	//12
+	//16
+	//14
 }
